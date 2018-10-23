473d51dbcaf4611b3e29471ba6f795e17ad42699
KrepyshevaVika <27vita03@mail.ru>
2018-10-23 22:54:57 +0300

    t

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import Pusher from 'pusher-js';
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:8080');
+
 
 class Slider extends Component {
+
     constructor(props) {
         super(props);
-    
+        
+
         this.state = {
             value: 50,
         }
     }
 
     componentDidMount() {
-        const pusher = new Pusher('APP_KEY');
-        const channel = pusher.subscribe('slider');
-        channel.bind('targetValue', data => {
-          this.setState({ value: data });
-        });
-      }
+        
+        socket.on('slider', (targetValue) => {
+            console.log(targetValue);
+            if (this.state.value !== targetValue.targetValue) {
 
-      handleChange(e) {
-        fetch("http://localhost:8080/slider", 
-                { method: "POST", body: JSON.stringify(e.target.value) });
-      }
+                this.refs.text.value = targetValue.targetValue;
+                //this.setState({ value: targetValue.targetValue });
+
+                console.log("re-render");
+            }
+        });
+    }
+
+    handleChange(e) {
+
+        let value = e.target.value;
+        function f(){
+            socket.emit('subscribeToSlider', value);
+        }
+        setTimeout(f, 200);
+    }
 
     render() {
         console.log("render");
@@ -30,7 +44,8 @@ class Slider extends Component {
             <div>
                 <Header active="Slider"/>
                 <div className="slider">
-                    <input type="range" min="0" max="100" value={this.state.value} onChange={(e) => this.handleChange(e)}/>
+
+                    <input type="range" min="0" max="100" ref="text" step="3" onChange={(e) => this.handleChange(e)}/>
                 </div>
             </div>
         );
